ac4542b8b1ba224f8ffb34e0fc533fc86c8496b4
Adan Garnica A <cuadrado015@gmail.com>
2022-05-19 17:43:33 -0500

    PCracion de Servicio

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,11 +10,14 @@ import { Component } from '@angular/core';
 export class PorPaisComponent{
   termino:string ="Hola Mundo";
 
-  constructor(){
+  constructor(private paisService: PaisService){
 
   }
 
   buscar(){
-
+    this.paisService.bucarPais(this.termino)
+    .subscribe(res => {
+      console.log(res)
+    });
   }
 }
